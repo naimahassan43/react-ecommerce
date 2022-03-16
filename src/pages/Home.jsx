1b599f843ed854ea481ddf767ff828f7e809dba3
Naima Hassan <naima.hassan434@gmail.com>
2022-03-16 10:48:13 +0000

    Navbar initialize & Styled components

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
 
 const Home = () => {
    return (
       <div>
-         Homepage
+         <Navbar/>
+         
       </div>
    )
 }

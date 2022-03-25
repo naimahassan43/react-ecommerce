3c00a5d171a5c20e74e68467ffae352da9cfd548
Naima Hassan <naima.hassan434@gmail.com>
2022-03-25 22:53:32 +0000

    Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,140 @@
+import React from "react";
+import styled from "styled-components";
+import {
+  Facebook,
+  Instagram,
+  Twitter,
+  Pinterest,
+  LocationOn,
+  Phone,
+  Mail,
+} from "@mui/icons-material";
+
+const Container = styled.div`
+  display: flex;
+`;
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+`;
+const Logo = styled.h1`
+  margin-bottom: 0px;
+`;
+
+const Desc = styled.p`
+  margin: 20px 0px;
+`;
+
+const SocialContainer = styled.div`
+  display: flex;
+`;
+
+const SocialIcon = styled.div`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  color: #fff;
+  background-color: #${(props) => props.color};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-right: 20px;
+`;
+
+const Center = styled.div`
+  flex: 1;
+  padding: 20px;
+`;
+
+const Title = styled.h3`
+  margin-bottom: 30px;
+`;
+const List = styled.ul`
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  display: flex;
+  flex-wrap: wrap;
+`;
+const ListItem = styled.li`
+  width: 50%;
+  margin-bottom: 10px;
+`;
+
+const Right = styled.div`
+  flex: 1;
+  padding: 20px;
+`;
+const ContactItem = styled.div`
+  margin-bottom: 20px;
+  display: flex;
+  align-items: center;
+`;
+const Payment = styled.img`
+  width: 50%;
+`;
+
+const Footer = () => {
+  return (
+    <Container>
+      <Left>
+        <Logo>NAIMA.</Logo>
+        <Desc>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed id
+          pharetra tellus. Integer ornare arcu ut augue mattis fermentum.
+          Aliquam luctus turpis in nunc finibus, a blandit enim ullamcorper.
+          Quisque ut accumsan libero. Sed nec quam tortor.
+        </Desc>
+        <SocialContainer>
+          <SocialIcon color="3B5999">
+            <Facebook />
+          </SocialIcon>
+          <SocialIcon color="E4405F">
+            <Instagram />
+          </SocialIcon>
+          <SocialIcon color="55ACEE">
+            <Twitter />
+          </SocialIcon>
+          <SocialIcon color="E60023">
+            <Pinterest />
+          </SocialIcon>
+        </SocialContainer>
+      </Left>
+      <Center>
+        <Title>Useful Links</Title>
+        <List>
+          <ListItem>Home</ListItem>
+          <ListItem>Cart</ListItem>
+          <ListItem>Woman Fashion</ListItem>
+          <ListItem>Man Fashion</ListItem>
+          <ListItem>Accessories</ListItem>
+          <ListItem>My Account</ListItem>
+          <ListItem>Order Tracking</ListItem>
+          <ListItem>Wishlist</ListItem>
+          <ListItem>Terms</ListItem>
+        </List>
+      </Center>
+      <Right>
+        <Title>Contact</Title>
+        <ContactItem>
+          {" "}
+          <LocationOn style={{ marginRight: "10px" }} /> 8 Larwood Place, MK6
+          2SA
+        </ContactItem>
+        <ContactItem>
+          {" "}
+          <Phone style={{ marginRight: "10px" }} /> +44 23423333
+        </ContactItem>
+        <ContactItem>
+          {" "}
+          <Mail style={{ marginRight: "10px" }} /> contact@example.com
+        </ContactItem>
+        <Payment src="https://i.ibb.co/Qfvn4z6/payment.png" />
+      </Right>
+    </Container>
+  );
+};
+
+export default Footer;

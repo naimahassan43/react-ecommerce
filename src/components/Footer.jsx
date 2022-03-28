d471bbb061c80feba782f6f49ccb2f428a9d805a
Naima Hassan <naima.hassan434@gmail.com>
2022-03-29 00:40:50 +0100

    Responsive Newsletter & Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,9 +9,11 @@ import {
   Phone,
   Mail,
 } from "@mui/icons-material";
+import { mobile } from "../responsive";
 
 const Container = styled.div`
   display: flex;
+  ${mobile({ flexDirection: "column" })}
 `;
 const Left = styled.div`
   flex: 1;
@@ -46,6 +48,7 @@ const SocialIcon = styled.div`
 const Center = styled.div`
   flex: 1;
   padding: 20px;
+  ${mobile({ display: "none" })}
 `;
 
 const Title = styled.h3`
@@ -66,6 +69,7 @@ const ListItem = styled.li`
 const Right = styled.div`
   flex: 1;
   padding: 20px;
+  ${mobile({ backgroundColor: "#fff8f8" })}
 `;
 const ContactItem = styled.div`
   margin-bottom: 20px;

32d46c4e7bc7347de88637e944635c5656c0ac50
Naima Hassan <naima.hassan434@gmail.com>
2022-03-28 01:45:10 +0100

    Register Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 // import Home from "./pages/Home"
 // import ProductList from "./pages/ProductList";
 
-import Product from "./pages/Product";
+import Register from "./pages/Register";
+
+// import Product from "./pages/Product";
 
 const App = () => {
   return (
     <>
       {/* <Home /> */}
       {/* <ProductList /> */}
-      <Product />
+      {/* <Product /> */}
+      <Register />
     </>
   );
 };

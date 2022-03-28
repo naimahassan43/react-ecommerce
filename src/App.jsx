49a01e2ae62783998d421ab2ab2c035bb5718152
Naima Hassan <naima.hassan434@gmail.com>
2022-03-29 00:50:20 +0100

    Responsive Product page

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 // import Home from "./pages/Home";
-import ProductList from "./pages/ProductList";
+// import ProductList from "./pages/ProductList";
 
 // import Register from "./pages/Register";
 // import Cart from "./pages/Cart";
 // import Login from "./pages/Login";
 
-// import Product from "./pages/Product";
+import Product from "./pages/Product";
 
 const App = () => {
   return (
     <>
       {/* <Home /> */}
-      <ProductList />
-      {/* <Product /> */}
+      {/* <ProductList /> */}
+      <Product />
       {/* <Login /> */}
       {/* <Register /> */}
       {/* <Cart /> */}
--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -4,11 +4,13 @@ import Announcement from "../components/Announcement";
 import Newsletter from "../components/Newsletter";
 import Footer from "../components/Footer";
 import { Add, Remove } from "@mui/icons-material";
+import { mobile } from "../responsive";
 
 const Container = styled.div``;
 const Wrapper = styled.div`
   padding: 50px;
   display: flex;
+  ${mobile({ padding: "10px", flexDirection: "column" })}
 `;
 const ImgContainer = styled.div`
   flex: 1;
@@ -17,10 +19,12 @@ const Image = styled.img`
   width: 100%;
   height: 90vh;
   object-fit: cover;
+  ${mobile({ height: "40vh" })}
 `;
 const InfoContainer = styled.div`
   flex: 1;
   padding: 0px 50px;
+  ${mobile({ padding: "10px" })}
 `;
 const Title = styled.h1`
   font-weight: 200;
@@ -32,12 +36,13 @@ const Price = styled.span`
   font-weight: 100;
   font-size: 40px;
 `;
-// const Price = styled.p``;
+
 const FilterContainer = styled.div`
   width: 50%;
   margin: 30px 0px;
   display: flex;
   justify-content: space-between;
+  ${mobile({ width: "100%" })}
 `;
 const Filter = styled.div`
   display: flex;
@@ -60,11 +65,13 @@ const FilterSize = styled.select`
   padding: 5px;
 `;
 const FilterSizeOption = styled.option``;
+
 const AddContainer = styled.div`
   width: 50%;
   display: flex;
   align-items: center;
   justify-content: space-between;
+  ${mobile({ width: "100%" })};
 `;
 const AmountContainer = styled.div`
   display: flex;

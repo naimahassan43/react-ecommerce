680718238f58377e292fa68af1a21a9dff05dcd2
Naima Hassan <naima.hassan434@gmail.com>
2022-03-29 00:12:27 +0100

    Responsive Navbar & slider

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-// import Home from "./pages/Home"
+import Home from "./pages/Home";
 // import ProductList from "./pages/ProductList";
 
 // import Register from "./pages/Register";
-import Cart from "./pages/Cart";
+// import Cart from "./pages/Cart";
 // import Login from "./pages/Login";
 
 // import Product from "./pages/Product";
@@ -10,12 +10,12 @@ import Cart from "./pages/Cart";
 const App = () => {
   return (
     <>
-      {/* <Home /> */}
+      <Home />
       {/* <ProductList /> */}
       {/* <Product /> */}
       {/* <Login /> */}
       {/* <Register /> */}
-      <Cart />
+      {/* <Cart /> */}
     </>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,82 +1,89 @@
-import { Search } from '@mui/icons-material'
-import React from 'react'
-import styled from 'styled-components'
-import Badge from '@mui/material/Badge';
-import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import { Search } from "@mui/icons-material";
+import React from "react";
+import styled from "styled-components";
+import Badge from "@mui/material/Badge";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import { mobile } from "../responsive";
 
 const Container = styled.div`
-      height:60px;   
-`
+  height: 60px;
+  ${mobile({ height: "50px" })};
+`;
 const Wrapper = styled.div`
-      padding: 10px 20px;
-      display: flex;
-      align-items: center;
-      justify-content:space-between;
-   `
+  padding: 10px 20px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  ${mobile({ padding: "10px 0px" })};
+`;
 const Left = styled.div`
-      flex: 1;
-      display: flex;
-      align-items: center;
-      `
+  flex: 1;
+  display: flex;
+  align-items: center;
+`;
 const Language = styled.span`
-      font-size: 14px;
-      cursor: pointer;
-`
+  font-size: 14px;
+  cursor: pointer;
+  ${mobile({ display: "none" })};
+`;
 const SearchContainer = styled.div`
-      border: 0.5px solid lightgray;
-      display: flex;
-      align-items: center;
-      margin-left: 16px;
-      padding:2px;
-`
+  border: 0.5px solid lightgray;
+  display: flex;
+  align-items: center;
+  margin-left: 16px;
+  padding: 2px;
+`;
 const Input = styled.input`
-      border:none;
-`
+  border: none;
+  ${mobile({ width: "50px" })}
+`;
 const Center = styled.div`
-      flex: 1;
-      text-align: center;
-      `
-const Logo = styled.h3`
-      margin-left: 10px;
-      
-`
+  flex: 1;
+  text-align: center;
+`;
+const Logo = styled.h1`
+  font-weight: bold;
+  ${mobile({ fontSize: "24px" })}
+`;
 const Right = styled.div`
-      flex: 1;
-      display: flex;
-      align-items: center;
-      justify-content: flex-end;
-      `
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  ${mobile({ flex: 2, justifyContent: "center" })};
+`;
 const MenuItem = styled.div`
-      font-size: 14px;
-      cursor: pointer;
-      margin-left: 25px;
-`
+  font-size: 14px;
+  cursor: pointer;
+  margin-left: 25px;
+  ${mobile({ fontSize: "12px", marginLeft: "10px" })}
+`;
 const Navbar = () => {
-   return (
-      <Container>
-         <Wrapper>
-            <Left>
-               <Language>EN</Language>
-               <SearchContainer>
-                  <Input/>
-                  <Search style={ {color: 'gray', fontSize: 14} }/>
-               </SearchContainer>
-            </Left>
-            <Center>
-               <Logo>NAIMA.</Logo>
-            </Center>
-            <Right>
-               <MenuItem>REGISTER</MenuItem>
-               <MenuItem>SIGN IN</MenuItem>
-               <MenuItem>
-                  <Badge badgeContent={4} color="secondary">
-                     <ShoppingCartOutlinedIcon/>
-                  </Badge>
-               </MenuItem>
-            </Right>
-         </Wrapper>
-      </Container>
-   )
-}
+  return (
+    <Container>
+      <Wrapper>
+        <Left>
+          <Language>EN</Language>
+          <SearchContainer>
+            <Input />
+            <Search style={{ color: "gray", fontSize: 14 }} />
+          </SearchContainer>
+        </Left>
+        <Center>
+          <Logo>NAIMA.</Logo>
+        </Center>
+        <Right>
+          <MenuItem>REGISTER</MenuItem>
+          <MenuItem>SIGN IN</MenuItem>
+          <MenuItem>
+            <Badge badgeContent={4} color="secondary">
+              <ShoppingCartOutlinedIcon />
+            </Badge>
+          </MenuItem>
+        </Right>
+      </Wrapper>
+    </Container>
+  );
+};
 
-export default Navbar
+export default Navbar;
